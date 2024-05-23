15de2449c07df7d281f1b12f708407c7e0071732
son0925 <shs00925@naver.com>
2024-05-23 22:23:29 +0900

    부트스트랩 css 설정

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+app.use(express.static(publicDirectoryPath));
+app.use(express.json());
+
+
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+})
