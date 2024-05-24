5c175c13cc495e2b4a042a5fc00146cf3fdfc264
son0925 <shs00925@naver.com>
2024-05-24 23:34:44 +0900

    userSession Create

--- public/main.js
+++ public/main.js
@@ -17,3 +17,43 @@ const userTagline = document.querySelector('#users-tagline');
 const title = document.querySelector('#active-user');
 const messages = document.querySelector('.messages');
 const msgDiv = document.querySelector('.msg-form');
+
+
+// login form handler
+const loginForm = document.querySelector('.user-login');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const username = document.getElementById('username');
+  createSession(username.value.toLowerCase());
+  username.value = '';
+})
+
+const createSession = async(username) => {
+  const option = {
+    method: 'Post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({username})
+  }
+  await fetch('/session', option)
+    .then((res) => res.json())
+    .then((data) => {
+      socketConnect(data.username, data.userId)
+      // localStorge 세션에 set
+      localStorage.setItem('session-username', data.username);
+      localStorage.setItem('session-userID', data.userId);
+
+      loginContainer.classList.add('d-none');
+      chatBody.classList.remove('d-none');
+      userTitle.innerHTML = data.username
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
+
+
+const socketConnect = async(username, userId) => {
+  socket.auth = {username, userId};
+
+  await socket.connect();
+}
