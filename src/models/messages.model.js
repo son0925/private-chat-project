b217b1696337a3c2cab3b7c77d5f5650914f19d8
son0925 <shs00925@naver.com>
2024-05-24 23:11:20 +0900

    몽고 모델 생성

---
+++ src/models/messages.model.js
@@ -0,0 +1,29 @@
+const { default: mongoose } = require("mongoose");
+
+const messagesSchema = mongoose.Schema({
+  userToken: {
+    type: String,
+    required: true
+  },
+  messages: [
+    {
+      from: {
+        type: String,
+        required: true
+      },
+      message: {
+        type: String,
+        required: true
+      },
+      time: {
+        type: String,
+        required: true
+      }
+    }
+  ]
+})
+
+
+const messageModel = mongoose.model('Message', messagesSchema);
+
+module.exports = messageModel;
